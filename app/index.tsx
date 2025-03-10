7a16417fc91e7d96ce7177b4f4c79f74cc0a66eb
kunal89204 <kunalkhandelwal108@gmail.com>
2025-03-10 13:43:17 +0530

    expo setup with native wind

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 justify-center items-center bg-gray-200"
+    >
+      <Text className="text-gray-500 text-xl">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
